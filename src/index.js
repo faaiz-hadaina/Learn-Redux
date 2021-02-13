a8be3f78055a1cb86019dbe2c1cab2db7ad1ff5a
unknown <faaiz.hadaina@gmail.com>
2021-02-13 13:44:45 +0100

    store

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { createStore } from 'redux'
 import allReducers from './reducers'
 import { Provider } from 'react-redux'
 
-const store = createStore(allReducers)
+const store = createStore(allReducers,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
  </React.StrictMode> ,
